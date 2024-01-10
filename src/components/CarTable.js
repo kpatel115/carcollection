4910e56973d9536e3d7fd745c8a11aaf86c6f5de
kpatel114 <kpatel114@hawk.iit.edu>
2024-01-10 15:38:54 -0600

    changed path in fetchCars

--- src/components/CarTable.js
+++ src/components/CarTable.js
@@ -19,7 +19,7 @@ const CarTable = () => {
     useEffect(() => {
         const fetchCars = async () => {
             try {
-                const response = await fetch('https://lustrous-smakager-5e4bf2.netlify.app/dashboard');
+                const response = await fetch('https://lustrous-smakager-5e4bf2.netlify.app/.netlify.functions.server/dashboard');
                 const data = await response.json();
                 setCars(data);
                 console.log('Fetched Cars', data)

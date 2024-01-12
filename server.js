ef578c3f269b6cb02f2ed81a1f982809b4d7344d
kpatel114 <kpatel114@hawk.iit.edu>
2024-01-12 14:54:16 -0600

    added a port

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const mongoURI = process.env.MONGODB_URI;
 
 
 const app = express();
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-const PORT = 'https://lustrous-smakager-5e4bf2.netlify.app/.netlify.functions.server'
+// const PORT = 'https://lustrous-smakager-5e4bf2.netlify.app/.netlify.functions.server'
 //Middleware
 app.use(cors());
 app.use(bodyParser.json());
